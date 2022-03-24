373c8856e446eb89768db1323df1576a8dc8505c
hooiyan <hoecoding@gmail.com>
2022-03-24 09:57:28 +0800

    Follow along instructor's solution for Layout Exercise 1

---
+++ app/screen/WelcomeScreen.js
@@ -0,0 +1,50 @@
+import React from "react"
+import { Image, ImageBackground, StyleSheet, Text, View } from "react-native"
+
+function WelcomeScreen() {
+  return (
+    <>
+      <ImageBackground
+        style={styles.background}
+        source={require("../assets/unsplash-example1.jpg")}>
+        <View style={styles.logoContainer}>
+          <Image
+            style={styles.logo}
+            source={require("../assets/favicon.png")}
+          />
+          <Text style={styles.tagline}>Sell What You Don't Need</Text>
+        </View>
+        <View style={styles.loginButton} />
+        <View style={styles.registerButton} />
+      </ImageBackground>
+    </>
+  )
+}
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  loginButton: {
+    backgroundColor: "#fc5c65",
+    height: 70,
+    width: "100%",
+  },
+  logoContainer: {
+    position: "absolute",
+    top: 70,
+    alignItems: "center",
+  },
+  registerButton: {
+    backgroundColor: "#4ecdc4",
+    height: 70,
+    width: "100%",
+  },
+  tagline: {
+    color: "white",
+  },
+})
+
+export default WelcomeScreen
