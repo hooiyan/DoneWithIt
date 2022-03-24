8dc79bfc9e1264d8d3a071113d3e72515533a7e0
hooiyan <hoecoding@gmail.com>
2022-03-24 15:40:49 +0800

    Check on iOS virtual device

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
+import { View } from "react-native"
 import { MaterialCommunityIcons } from "@expo/vector-icons"
 import AppText from "./app/components/AppText"
+import ViewImageScreen from "./app/screen/ViewImageScreen"
 
 export default function App() {
   return (
-    <View>
+    <View
+      style={{
+        display: "flex",
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}>
       <AppText>Hello React Native</AppText>
       <MaterialCommunityIcons name="email" />
+      <ViewImageScreen />
     </View>
   )
 }
